7f4a2cc50bf5bc44745bdf5681fcceff0e3174b2
Krutika <krutikavk@gmail.com>
2020-09-15 23:10:35 -0700

    Adding eslint airbnb styles

---
+++ backend/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+
+  // eslint-disable-next-line quote-props
+  'env': {
+    // eslint-disable-next-line quote-props
+    'browser': true,
+    // eslint-disable-next-line quote-props
+    'es2021': true,
+  },
+  // eslint-disable-next-line quote-props
+  'extends': [
+    'airbnb-base',
+  ],
+  // eslint-disable-next-line quote-props
+  'parserOptions': {
+    // eslint-disable-next-line quote-props
+    'ecmaVersion': 12,
+    // eslint-disable-next-line quote-props
+    'sourceType': 'module',
+  },
+  // eslint-disable-next-line quote-props
+  'rules': {
+  },
+};
