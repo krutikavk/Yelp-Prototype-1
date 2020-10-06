983293d610c5abb08bc4c47f05503c5504a97bfb
Krutika <krutikavk@gmail.com>
2020-10-06 01:05:13 -0700

    AWS s3 integration

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,10 @@ const app = express();
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
-//const mysql = require('mysql');
+var aws = require('aws-sdk');
+require('dotenv').config();
 const cors = require('cors');
-//const sql = require('sql-template-strings')
-//app.set('view engine', 'ejs');
+
 
 //use cors to allow cross origin resource sharing
 app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
@@ -21,20 +21,25 @@ app.use(session({
     activeDuration      : 5 * 60 * 1000
 }));
 
-// app.use(bodyParser.urlencoded({
-//     extended: true
-//   }));
+
 app.use(bodyParser.json());
 
 //Allow Access Control
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
-    res.setHeader('Cache-Control', 'no-cache');
-    next();
-  });
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+  res.setHeader('Cache-Control', 'no-cache');
+  next();
+});
+
+aws.config.update({
+  region: process.env.region, // Put your aws region here
+  accessKeyId: process.env.AWSAccessKeyId,
+  secretAccessKey: process.env.AWSSecretKey,
+  signatureVersion:'v4'
+})
 
 var customers = require('./customers.js')
 var restaurants = require('./restaurants.js')
@@ -46,139 +51,53 @@ app.use('/restaurants', restaurants);
 app.use('/orders', orders);
 app.use('/dishes', dishes);
 
-app.get('*', function(request, response){
-  console.log('Page not found');
-  response.status('404');
+
+
+//reference : https://medium.com/@khelif96/uploading-files-from-a-react-app-to-aws-s3-the-right-way-541dd6be689
+// Now lets export this function so we can call it from somewhere else
+app.post('/sign_s3', function (req, res) {
+   const s3 = new aws.S3();  // Create a new instance of S3
+   const S3_BUCKET = process.env.Bucket
+   const fileName = req.body.fileName;
+   const fileType = req.body.fileType;
+   console.log(req.body);
+   // Set up the payload of what we are sending to the S3 api
+   
+   const s3Params = {
+            Bucket: S3_BUCKET,
+            Key: fileName,
+            Expires: 500,
+            ContentType: fileType,
+            ACL: 'public-read',
+            };
+// Make a request to the S3 API to get a signed URL which we can use to upload our file
+   s3.getSignedUrl('putObject', s3Params, (err, data) => {
+            if(err){
+              console.log(err);
+              res.json({success: false, error: err})
+              }
+   // Data payload of what we are sending back, the url of the signedRequest and a URL where we can access the content after its saved. 
+   console.log(data)
+   const returnData = {
+    signedRequest: data,
+    url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+    };
+    // Send it all back
+    res.json({success:true, data:{returnData}});
+    });
 });
-
-
-//start backend server on port 3001
-app.listen(3001);
-console.log("Backend Server listening on port 3001");
 
 /*
-
-
-//Route to handle Post Request Call
-app.post('/login', (request,response) => {
-    
-    console.log('Inside Login Post Request');
-    //console.log("Req Body : ", username + "password : ",password);
-    console.log('Req Body : ',request.body);
-
-    if(request.body.loginOption === 'customer') {
-      var dbQuery = (sql `SELECT * from customer WHERE cemail = ? AND cpassword = ?`);
-      connection.query(dbQuery, [request.body.username, request.body.password], function(error, results) {
-      if(error) {
-        response.status(404).send('Could not fetch from database');
-      }
-      if (results.length > 0) {
-        response.cookie('cookie','customer',{maxAge: 900000, httpOnly: false, path : '/'});
-        request.session.user = request.body.username;
-        response.writeHead(200,{
-            //'Content-Type' : 'text/plain'
-            'Content-Type': 'application/json'
-
-        })
-        //response.end("Successful Login");
-        response.end(JSON.stringify(results));
-        } else {
-          response.status(404).send('Incorrect login');
-        }     
-      });
-    }
-
-    if(request.body.loginOption === 'restaurant') {
-      var dbQuery = (sql `SELECT * from restaurant WHERE remail = ? AND rpassword = ?`);
-      connection.query(dbQuery, [request.body.username, request.body.password], function(error, results) {
-      if(error) {
-        response.status(404).send('Could not fetch from database');
-      }
-      if (results.length > 0) {
-        response.cookie('cookie','customer',{maxAge: 900000, httpOnly: false, path : '/'});
-        request.session.user = request.body.username;
-        response.writeHead(200,{
-            'Content-Type' : 'text/plain'
-            //'Content-Type': 'application/json'
-
-        })
-        response.end("Successful Login");
-        //response.end(JSON.stringify(results));
-      } else {
-        response.status(404).send('Incorrect login');
-      }     
-    });
-    }
-});
-
-app.post('/custsignup', (request, response) => {
-    console.log("Inside customer signup post")
-    console.log("Req Body: ", request.body)
-
-    var dbQuery = (sql `INSERT into customer (cname, cemail, cpassword) VALUES (?, ?, ?)`);
-
-    connection.query(dbQuery, [request.body.cname, request.body.cemail, request.body.cpassword], function(error, results) {
-      if(error) {
-        console.log('User already exists')
-        response.status(404).send('User may already exist');
-
-      } else {
-        //also let the customer login
-        console.log("cust signup insert succeeded")
-        response.cookie('cookie','customer',{maxAge: 900000, httpOnly: false, path : '/'});
-        request.session.user = request.body.username;
-        let getUserQuery = (sql `SELECT * from customer WHERE cemail = ?`);
-        connection.query(getUserQuery, [request.body.cemail], function(error, results) {
-          if(error) {
-            response.status(404).send('Could not fetch from database');
-          }
-          if (results.length > 0) {
-            response.writeHead(200,{
-                //'Content-Type' : 'text/plain'
-                'Content-Type': 'application/json'
-
-            })
-            //response.end("Successful Login");
-            response.end(JSON.stringify(results));
-            } else {
-              response.status(404).send('Incorrect login');
-            }     
-        });
-      }
-
-    });
-});
-
-app.put('/custUpdate', function (request, response) {
-    let updateUserQuery = (sql `UPDATE customer SET cpassword = ?, cname = ? where cemail = ?`);
-    connection.query(updateUserQuery, [request.body.cpassword,request.body.cname, request.body.cemail], function (error, results, fields) {
-      if (error) {
-        console.log('Could not update the resource')
-        response.status(404).send('Could not update the resource'); 
-      } else {
-        response.writeHead(200,{
-          'Content-Type' : 'text/plain'
-        })
-        response.end("Successfully updated");
-      }
-  });
-});
-
-//Route to get All Books when user visits the Home Page
-app.get('/home', function(request,reesponse){
-    console.log("Inside Home Login");    
-    response.writeHead(200, {
-        'Content-Type' : 'text/plain'
-    })
-    response.end('Default view without login')
-    
-})
-
-
 app.get('*', function(request, response){
   console.log('Page not found');
   response.status('404');
 });
 */
 
+
+
+
+//start backend server on port 3001
+app.listen(3001);
+console.log("Backend Server listening on port 3001");
 
--- frontend/src/Components/Userdash/userdash.js
+++ frontend/src/Components/Userdash/userdash.js
@@ -21,6 +21,8 @@ class Userdash extends Component {
 	  password: '',
 	  usernameToChange: false,
 	  passwordToChange: false,
+    url: '',
+    success: false,
 	  errors: {
 	  	username: '',
 	  	password: '',
@@ -33,6 +35,8 @@ class Userdash extends Component {
     this.submitPasswordChange = this.submitPasswordChange.bind(this);
     this.usernameEditTextFieldHandler = this.usernameEditTextFieldHandler.bind(this);
     this.passwordEditTextFieldHandler = this.passwordEditTextFieldHandler.bind(this);
+    this.handleFileUpload = this.handleFileUpload.bind(this);
+    this.handleFileChange = this.handleFileChange.bind(this);
   }
 
   componentWillMount() {
@@ -53,8 +57,6 @@ class Userdash extends Component {
     	passwordToChange: true,
     })
   }
-
-
 
   usernameChangeHandler = (event) => {
   	let err = this.state.errors;
@@ -147,6 +149,58 @@ class Userdash extends Component {
   }
 
 
+  handleFileChange = (ev) => {
+    this.setState({
+      success: false, url : ''
+    });
+  }
+
+  // Perform the upload
+  handleFileUpload = (ev) => {
+    this.setState({success: false, url : ""});
+    let file = this.uploadInput.files[0];
+    // Split the filename to get the name and type
+    let fileParts = this.uploadInput.files[0].name.split('.');
+    let fileName = 'custprof_' + fileParts[0];
+    console.log(fileName);
+    let fileType = fileParts[1];
+    console.log("Preparing the upload");
+
+    axios.defaults.withCredentials = true;
+
+    axios.post("http://localhost:3001/sign_s3",{
+               fileName : fileName,
+               fileType : fileType
+               })
+               .then(response => {
+               var returnData = response.data.data.returnData;
+               var signedRequest = returnData.signedRequest;
+               var url = returnData.url;
+               this.setState({url: url})
+               this.setState({success: true})
+    console.log("Recieved a signed request " + signedRequest);
+    // Put the fileType in the headers for the upload
+    var options = {
+      headers: {
+        'ContentType': fileType
+      }
+    };
+
+    axios.put(signedRequest,file, options)
+         .then(result => {
+           console.log("Response from s3")
+           this.setState({success: true});
+         })
+         .catch(error => {
+           alert("ERROR " + JSON.stringify(error));
+         })
+    })
+    .catch(error => {
+      alert(JSON.stringify(error));
+    })
+  }
+
+
   render() {
   	let redirectVar = null;
   	console.log(this.props.isLogged)
@@ -190,34 +244,51 @@ class Userdash extends Component {
       )
   	}
 
+
+    const Success_message = () => (
+      <div class='form-group'>
+      <img src={this.state.url} class="img-responsive" width="40" height="40"/>
+      <br/>
+      </div>
+    )
+
     return (
 
       <div>
       	{redirectVar}
-	    <div class='dashboard'>
-		    <div class='form-group'>
-			  USERNAME: {this.props.cname}
-			  {usernameTextField}
+  	    <div class='dashboard'>
+  		    <div class='form-group'>
+  			  USERNAME: {this.props.cname}
+  			  {usernameTextField}
 
-		    </div>
-		    <div class='form-group'>
-		      PASSWORD
-		      {passwordTextField}
+  		    </div>
+  		    <div class='form-group'>
+  		      PASSWORD
+  		      {passwordTextField}
 
-		    </div>
-		    <div class='form-group'>
-			  EMAIL ID: {this.props.cemail}
-			</div>
-			<div>
-			  PROFILE PICTURE
-			  <div>
-		    	<img src={profilepicture} alt = "" width="300px" height="200px"></img>
-			    <h2>User Dashboard</h2>
-		      </div>
-			</div>
-		  </div>
-	  </div>
-	)
+  		    </div>
+  		    <div class='form-group'>
+  			  EMAIL ID: {this.props.cemail}
+  			</div>
+  			<div>
+  			  PROFILE PICTURE
+  			  <div>
+  		    	<img src={profilepicture} alt = "" width="300px" height="200px"></img>
+  			    <h2>User Dashboard</h2>
+  		      </div>
+  			 </div>
+         
+         <div class="form-group">
+            {this.state.success ? <Success_message /> : null}
+            <input onChange={this.handleFileUpload} ref = {(ref) => {this.uploadInput = ref;}} type = "file"/>
+          </div>
+
+
+
+
+  		  </div>
+  	  </div>
+  	)
   }
 
 }
